a62af955c8f895ef7734b5635f44ee4657b37c7d
JoeyShapiro <joeyashapiro@gmail.com>
2025-05-04 15:06:30 -0500

    cleanup

--- statCUE.cpp
+++ statCUE.cpp
@@ -6,6 +6,8 @@
 #include <d3dcompiler.h>
 
 static CorsairDeviceInfo corsair_ram;
+static CorsairLedPosition* leds = nullptr;
+static CorsairLedColor* colors = nullptr;
 
 struct ShaderParameters {
     UINT ledCount;
@@ -24,6 +26,9 @@ struct Color {
 void stateChange(void* context, const CorsairSessionStateChanged* event) {
 	if (event->state != CSS_Connected) return;
 
+	if (leds) free(leds);
+	if (colors) free(colors);
+
 	CorsairDeviceFilter filter = { 0 };
 	filter.deviceTypeMask = CDT_All;// CDT_MemoryModule;
 	int size = 0;
@@ -39,6 +44,24 @@ void stateChange(void* context, const CorsairSessionStateChanged* event) {
 		if (devices[i].type != CDT_MemoryModule) continue;
 
 		corsair_ram = devices[i];
+
+		leds = (CorsairLedPosition*)malloc(sizeof(CorsairLedPosition) * corsair_ram.ledCount);
+		int size = 0;
+		err = CorsairGetLedPositions(corsair_ram.id, CORSAIR_DEVICE_LEDCOUNT_MAX, leds, &size);
+		if (err != CE_Success) {
+			MessageBoxA(NULL, "Failed to get LED positions", "Error", MB_ICONERROR | MB_OK);
+			return;
+		}
+		if (size != corsair_ram.ledCount) {
+			MessageBoxA(NULL, "Failed to get LED positions", "Error", MB_ICONERROR | MB_OK);
+			return;
+		}
+
+		colors = (CorsairLedColor*)malloc(sizeof(CorsairLedColor) * corsair_ram.ledCount);
+		if (!colors) {
+			MessageBoxA(NULL, "This error is here because, somehow, malloc failed. Download more RAM lol", "Error", MB_ICONERROR | MB_OK);
+			return;
+		}
 	}
 }
 
@@ -208,97 +231,79 @@ int main() {
     MEMORYSTATUSEX memInfo;
 	memInfo.dwLength = sizeof(MEMORYSTATUSEX);
 	while (true) {
+		Sleep(16);
+		if (!corsair_ram.id[0]) continue;
+		
 		GlobalMemoryStatusEx(&memInfo);
 
-		float used = memInfo.dwMemoryLoad / static_cast<float>(100);
+		// Update parameters
+		ShaderParameters params = {
+			.ledCount = corsair_ram.ledCount,
+			.usage = memInfo.dwMemoryLoad / static_cast<float>(100),
+			.ticks = GetTickCount(),
+			.corsairDeviceType = corsair_ram.type
+		};
 
-		if (corsair_ram.id[0]) {
-            // Update parameters
-            ShaderParameters params = {};
+		D3D11_MAPPED_SUBRESOURCE mappedResource;
+		hr = context->Map(constantBuffer, 0, D3D11_MAP_WRITE_DISCARD, 0, &mappedResource);
+		if (FAILED(hr)) {
+			MessageBoxA(NULL, "Context failed to map constant resources", "Error", MB_ICONERROR | MB_OK);
 
-            params.ledCount = corsair_ram.ledCount;
-            params.usage = used;
-            params.ticks = GetTickCount();
-            params.corsairDeviceType = corsair_ram.type;
-
-            D3D11_MAPPED_SUBRESOURCE mappedResource;
-            hr = context->Map(constantBuffer, 0, D3D11_MAP_WRITE_DISCARD, 0, &mappedResource);
-
-            if (SUCCEEDED(hr)) {
-                memcpy(mappedResource.pData, &params, sizeof(ShaderParameters));
-                context->Unmap(constantBuffer, 0);
-            }
-            else {
-                MessageBoxA(NULL, "Context failed to map constant resources", "Error", MB_ICONERROR | MB_OK);
-
-                stagingBuffer->Release();
-                constantBuffer->Release();
-                outputUAV->Release();
-                outputBuffer->Release();
-                computeShader->Release();
-                if (device) device->Release();
-                if (context) context->Release();
-                return 1;
-            }
-
-            // Set up compute shader
-            context->CSSetShader(computeShader, nullptr, 0);
-            context->CSSetConstantBuffers(0, 1, &constantBuffer);
-            context->CSSetUnorderedAccessViews(0, 1, &outputUAV, nullptr);
-
-            // Dispatch compute shader
-            // Calculate how many thread groups to dispatch (each group has 256 threads)
-            UINT threadGroupCount = (20 + 255) / 256;
-            context->Dispatch(threadGroupCount, 1, 1);
-
-            // Unbind resources
-            ID3D11UnorderedAccessView* nullUAV = nullptr;
-            context->CSSetUnorderedAccessViews(0, 1, &nullUAV, nullptr);
-
-            // Copy results to staging buffer for reading
-            context->CopyResource(stagingBuffer, outputBuffer);
-
-            // Read back results
-            hr = context->Map(stagingBuffer, 0, D3D11_MAP_READ, 0, &mappedResource);
-
-            CorsairLedPosition* leds = (CorsairLedPosition*)malloc(sizeof(CorsairLedPosition) * corsair_ram.ledCount);
-            int size = 0;
-            err = CorsairGetLedPositions(corsair_ram.id, CORSAIR_DEVICE_LEDCOUNT_MAX, leds, &size);
-            if (!leds) {
-                MessageBoxA(NULL, "Corsair failed to get device LEDs", "Error", MB_ICONERROR | MB_OK);
-                return 1;
-            }
-
-            CorsairLedColor* colors = (CorsairLedColor*)malloc(sizeof(CorsairLedColor) * corsair_ram.ledCount);
-            if (!colors) {
-                MessageBoxA(NULL, "This error is here because, somehow, malloc failed. Download more RAM lol", "Error", MB_ICONERROR | MB_OK);
-                return 1;
-            }
-            ZeroMemory(colors, sizeof(CorsairLedColor) * corsair_ram.ledCount);
-
-            if (SUCCEEDED(hr)) {
-                // Get data pointer
-                Color* data = static_cast<Color*>(mappedResource.pData);
-
-                // Process data with C library function
-                for (size_t i = 0; i < corsair_ram.ledCount; i++)
-                {
-                    colors[i].id = leds[i].id;
-                    colors[i].r = data[i].r;
-                    colors[i].g = data[i].g;
-                    colors[i].a = data[i].a;
-                }
-
-                context->Unmap(stagingBuffer, 0);
-            }
-            else {
-                MessageBoxA(NULL, "Failed to read staging buffer", "Error", MB_ICONERROR | MB_OK);
-                return 1;
-            }
-
-			err = CorsairSetLedColors(corsair_ram.id, corsair_ram.ledCount, colors);
+			stagingBuffer->Release();
+			constantBuffer->Release();
+			outputUAV->Release();
+			outputBuffer->Release();
+			computeShader->Release();
+			if (device) device->Release();
+			if (context) context->Release();
+			return 1;
 		}
-		Sleep(16);
+
+		memcpy(mappedResource.pData, &params, sizeof(ShaderParameters));
+		context->Unmap(constantBuffer, 0);
+
+		// Set up compute shader
+		context->CSSetShader(computeShader, nullptr, 0);
+		context->CSSetConstantBuffers(0, 1, &constantBuffer);
+		context->CSSetUnorderedAccessViews(0, 1, &outputUAV, nullptr);
+
+		// Dispatch compute shader
+		// Calculate how many thread groups to dispatch (each group has 256 threads)
+		UINT threadGroupCount = (20 + 255) / 256;
+		context->Dispatch(threadGroupCount, 1, 1);
+
+		// Unbind resources
+		ID3D11UnorderedAccessView* nullUAV = nullptr;
+		context->CSSetUnorderedAccessViews(0, 1, &nullUAV, nullptr);
+
+		// Copy results to staging buffer for reading
+		context->CopyResource(stagingBuffer, outputBuffer);
+
+		// Read back results
+		hr = context->Map(stagingBuffer, 0, D3D11_MAP_READ, 0, &mappedResource);
+		if (FAILED(hr)) {
+			MessageBoxA(NULL, "Failed to read staging buffer", "Error", MB_ICONERROR | MB_OK);
+			return 1;
+		}
+
+		if (!leds) {
+			MessageBoxA(NULL, "Corsair failed to get device LEDs", "Error", MB_ICONERROR | MB_OK);
+			return 1;
+		}
+
+		// Get data pointer
+		Color* data = static_cast<Color*>(mappedResource.pData);
+		for (size_t i = 0; i < corsair_ram.ledCount; i++)
+		{
+			colors[i].id = leds[i].id;
+			colors[i].r = data[i].r;
+			colors[i].g = data[i].g;
+			colors[i].a = data[i].a;
+		}
+
+		context->Unmap(stagingBuffer, 0);
+
+		err = CorsairSetLedColors(corsair_ram.id, corsair_ram.ledCount, colors);
 	}
 
     // Clean up resources
